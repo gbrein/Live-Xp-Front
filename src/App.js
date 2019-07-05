4974d28e32ee446b2e02e3fcce48305d41eef8df
gbrein <gbrein@gmail.com>
2019-07-04 21:12:30 -0300

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import Navi from './Components/Navi';
+import Home from './Components/Home';
+import FooterPage from './Components/FooterPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navi sticky="top"/>
+            <Switch>
+                <Route
+                    exact
+                    path='/'
+                    render=
+                    { ()=> <Home />}/>
+            </Switch>
+            <FooterPage />
+        </div>
+    );
 }
 
 export default App;
