1baa68cc53d4721f694385180dcb84bbc1c986a3
gbrein <gbrein@gmail.com>
2019-07-30 18:17:37 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Client from './views/Client';
 import Logout from './views/Logout';
 import axios from 'axios';
 
-const http = axios.create({baseURL: 'http://127.0.0.1:3000/'})
+const http = axios.create({baseURL: 'https://aqueous-plains-46792.herokuapp.com//'})
 
 class App extends Component {
     constructor() {
